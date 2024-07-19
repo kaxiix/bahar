5de61a64e7d84674da3212b2b420db47ff434ccf
kaxiix <85871821+kaxiix@users.noreply.github.com>
2024-07-19 22:02:24 +0400

    Your commit message

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+// app/about/page.tsx
+
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>alhamdulillah</p>
+    </div>
+  );
+};
+
+export default AboutPage;
